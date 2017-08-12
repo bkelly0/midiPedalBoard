97a90daf859e5daae11dc850a79814585aca1a8a
bkelly0 <brad.kelly@sonydadc.com>
2017-08-12 15:01:43 -0400

    sensor state monitoring and config

--- midiPedalBoard.ino
+++ midiPedalBoard.ino
@@ -2,11 +2,20 @@
 #include <LiquidCrystal_I2C.h>
 
 //TODO: test and adjust
-#define SENSOR_THREASHOLD 100
+#define SENSOR_THREASHOLD 200
 #define MODE_SINGLE_NOTE  0
-#define TRACKING_STEPS 4
+#define TRACKING_STEPS 10
+#define MIN_TOTAL 2000.0
+#define MAX_TOTAL 8000.0
 
+#define SENSOR_BASELINE 0
+#define SENSOR_RISING 1
+#define SENSOR_HOLDING 2
+#define SENSOR_FALLING 3
+
+boolean calibrationMode = false;
 boolean configMode = false;
+long calibrationTime = 0;
 
 byte major = 0;
 byte minor = 1;
@@ -17,21 +26,30 @@ class Note {
   public:
     int mpNoteChannel, mpLedChannel;
     String name;
-    boolean isOn = false;
-    long trackingStartTime = 0;
-    long trackingCount = 0;
-    long trackingTotal = 0;
+    byte sensorState = SENSOR_BASELINE;
+    int stateChangeCount = 0;
+    long peakReading = 0;
+    long baseline = 0;
+    long threashold = 0;
+    float velocity;
+
     Note() {}
     Note(int _mpNoteChannel, int _mpLedChannel, String _name) {
       mpNoteChannel = _mpNoteChannel;
       mpLedChannel = _mpLedChannel;
       name = _name;
     }
+    void updateBaseline(long value) {
+        baseline = value;
+        threashold = value + 10;
+    }
     void reset() {
-      isOn = false;
-      trackingStartTime = 0;
-      trackingCount = 0;
-      trackingTotal = 0;
+      peakReading = 0;
+    }
+    long getVelocity() {
+      //100 seems to be the average peak for my sensors
+      velocity = (peakReading - baseline) / 100.0;
+      return velocity;
     }
 };
 
@@ -58,18 +76,21 @@ class Multiplexer {
   int sig, s0, s1, s2, s3;
   public:
   Multiplexer() {}
-  Multiplexer(int sig, int _s0, int _s1, int _s2, int _s3) {
+  Multiplexer(int _sig, int _s0, int _s1, int _s2, int _s3) {
     s0 = _s0;
     s1 = _s1;
     s2 = _s2;
     s3 = _s3;
+    sig = _sig;
+  }
+  void init() {
     pinMode(s0, OUTPUT);
     pinMode(s1, OUTPUT);
     pinMode(s2, OUTPUT);
     pinMode(s3, OUTPUT);
     pinMode(sig, INPUT);
   }
-  long analogRead(int channel) {
+  long analogReadChannel(int channel) {
     selectChannel(channel);
     return analogRead(sig);
   }
@@ -102,35 +123,58 @@ class Settings {
 Settings settings = Settings();
 
 Note notes[13];
-Multiplexer sensorMp = Multiplexer(A0,1,2,3,4);
-LiquidCrystal_I2C  lcd(0x27,16,2);
+Multiplexer sensorMp = Multiplexer(A0,5,4,3,2);
+//http://henrysbench.capnfatz.com/henrys-bench/arduino-projects-tips-and-more/arduino-quick-tip-find-your-i2c-address/
+LiquidCrystal_I2C  lcd(0x3F,16, 2);
 
 long ms;//current time
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("init");
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
   String noteNames[13] = {"C", "C#","D","D#","E","F","F#","G","G#","A","A#","B","C2"};
   for (int i=0; i<13; i++) {
     notes[i] = Note(i,i,noteNames[i]);
   }
 
+  sensorMp.init();
   initLCD();
-  writeLCD("Mode: " + String(settings.mode.name), "Ch: " + String(settings.midiChannel));
+  writeLCD("Mode: " + String(settings.mode.name), "Ch: " + String(settings.midiChannel) + " Oct:" + String(settings.octave));
 }
 
 void loop() {
   ms = millis();
-  if (!configMode) {
-    readSensors();
+  if (calibrationMode) {
+    if (calibrationTime == 0) {
+      calibrationTime = ms + 10000;
+    }
+    collectSensorCalibrationData();
+    if (ms >= calibrationTime) {
+      calibrationTime = 0;
+      for (int i=0; i<2; i++) {
+        Serial.print(notes[i].peakReading);
+        Serial.print(" ");
+        Serial.print(notes[i].baseline);
+        Serial.println();
+      }
+    }
+   
+  } else {
+    if (!configMode) {
+      readSensors();
+    }  
   }
-  tempCount ++;
-  Serial.println((ms/1000) + " " + tempCount);
 }
 
 void initLCD() {
-  lcd.begin();
+  lcd.init();
+  lcd.init();
   lcd.backlight();
-  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Starting...");
 }
 
 void writeLCD(String line1, String line2) {
@@ -138,33 +182,96 @@ void writeLCD(String line1, String line2) {
   lcd.print(line1);
   lcd.setCursor(0,1);
   lcd.print(line2);
-  delay(1000);
+}
+
+void debugDifference(long difference) {
+    Serial.print(difference);
+    Serial.print(" ");
+    Serial.println();
+}
+
+void collectConfigData() {
+  
 }
 
 void readSensors() {
-  for (int i=0; i<13; i++) {
-    Note note = notes[i];
-    long value = sensorMp.analogRead(note.mpNoteChannel);
-    if (!note.isOn && value >= SENSOR_THREASHOLD) {
-      if (note.trackingStartTime == 0) {
-        note.trackingStartTime = ms;
-        note.trackingTotal = value;
-        note.trackingCount++;
-      } else {
-        note.trackingTotal += value;
-        note.trackingCount++;
-        if (note.trackingCount == TRACKING_STEPS) {
-          //TODO: calculate velocity and send midi on
-        }
+  bool debug = true;
+  int i;
+  for (i=0; i<12; i++) {
+    long value = sensorMp.analogReadChannel(notes[i].mpNoteChannel);
+    long difference = value - notes[i].baseline;
+
+    if (debug) {
+      Serial.print(value);
+      Serial.print(" ");
+      Serial.print(notes[i].velocity);
+      //Serial.print(notes[i].sensorState*100);
+      Serial.print(" ");
+    }
+    
+    if (notes[i].sensorState == SENSOR_BASELINE && notes[i].baseline > 0 && difference > 15) {
+        notes[i].sensorState = SENSOR_RISING;
+    
+    } else if (notes[i].sensorState == SENSOR_RISING) {
+      notes[i].stateChangeCount++;
+      
+      if (value > notes[i].peakReading && notes[i].stateChangeCount < 3) {
+        notes[i].peakReading = value; //still rising
+      } else if (value <= notes[i].peakReading || notes[i].stateChangeCount == 3) {
+          digitalWrite(13, HIGH); 
+          notes[i].stateChangeCount = 0;
+          notes[i].sensorState = SENSOR_HOLDING;
+          notes[i].getVelocity();
       }
-    } else if (note.isOn && value < SENSOR_THREASHOLD) {
-      stopNote(i);
+
+    
+    } else if (notes[i].sensorState == SENSOR_HOLDING) {
+       if(value < notes[i].peakReading - 10) {
+          notes[i].stateChangeCount++;
+          if (notes[i].stateChangeCount == 2) {
+            notes[i].sensorState = SENSOR_FALLING;
+            notes[i].peakReading = 0;
+            notes[i].stateChangeCount = 0;
+            stopNote(i);
+          }
+       } else {
+         notes[i].stateChangeCount = 0;
+       }
+       
+    } else if (notes[i].sensorState == SENSOR_FALLING) {
+       if (value <= notes[i].baseline) {
+          notes[i].updateBaseline(value);
+          notes[i].sensorState = SENSOR_BASELINE;
+          notes[i].velocity = 0;
+       }
+  
+    } else {
+      notes[i].updateBaseline(value);
     }
   }
+  if (debug) {
+      Serial.println();
+  }
+}
+
+void collectSensorCalibrationData() {
+    for (int i=0; i<12; i++) {
+      long value = sensorMp.analogReadChannel(notes[i].mpNoteChannel);
+      if (notes[i].baseline == 0) {
+        notes[i].baseline = value;
+      }
+      if (value > notes[i].peakReading) {
+        notes[i].peakReading = value;
+      } else if (value < notes[i].baseline) {
+        notes[i].baseline = value;
+      }
+      
+    }
 }
 
 void stopNote(int index) {
   //TODO: stop midi note
   notes[index].reset();
+  digitalWrite(13, LOW);
 }
 
